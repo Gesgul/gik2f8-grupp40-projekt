48cbd6f789a34ee902b7b7910edc869f6e4627ab
Gulled Gesey <gulledgeesey@hotmail.com>
2023-01-06 16:09:44 +0000

    last

--- 02-todo/server/app.js
+++ 02-todo/server/app.js
@@ -6,7 +6,7 @@ const app = express();
 /* Importerar den inbyggda modulen fs */
 const fs = require('fs/promises');
 
-const PORT = 5501;
+const PORT = 5000;
 /* Expressobjektet, kallat app, har metoden "use" som används för att sätta inställningar hos vår server */
 app
   /* Man kan ange format etc. på de data som servern ska kunna ta emot och skicka. Metoderna json och urlencoded är inbyggda hos express */
@@ -142,4 +142,4 @@ app.put('/tasks/:id', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log('Server running on http://localhost:5500'));
+app.listen(PORT, () => console.log('Server running on http://localhost:5000'));
