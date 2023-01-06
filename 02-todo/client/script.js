ed2b2cc54c115ba6483635433b20862253f0e72e
Gulled Gesey <gulledgeesey@hotmail.com>
2023-01-06 16:24:23 +0000

    last

--- 02-todo/client/script.js
+++ 02-todo/client/script.js
@@ -1,52 +1,40 @@
-todoForm.title.addEventListener('keyup', (e) => validateField(e.target));
-todoForm.title.addEventListener('blur', (e) => validateField(e.target));
-todoForm.description.addEventListener('input', (e) => validateField(e.target));
-todoForm.description.addEventListener('blur', (e) => validateField(e.target));
+todoForm.title.addEventListener("keyup", (e) => validateField(e.target));
+todoForm.title.addEventListener("blur", (e) => validateField(e.target));
+todoForm.description.addEventListener("input", (e) => validateField(e.target));
+todoForm.description.addEventListener("blur", (e) => validateField(e.target));
 
-todoForm.dueDate.addEventListener('input', (e) => validateField(e.target));
-todoForm.dueDate.addEventListener('blur', (e) => validateField(e.target));
-todoForm.addEventListener('submit', onSubmit);
-const todoListElement = document.getElementById('todoList');
+todoForm.dueDate.addEventListener("input", (e) => validateField(e.target));
+todoForm.dueDate.addEventListener("blur", (e) => validateField(e.target));
+
+todoForm.addEventListener("submit", onSubmit);
+
+const todoListElement = document.getElementById("todoList");
+
 let titleValid = true;
 let descriptionValid = true;
 let dueDateValid = true;
-const api = new Api('http://localhost:5000/tasks');
 
+const api = new Api("http://localhost:5000/tasks");
 
 function validateField(field) {
-  /* Destructuring används för att plocka ut endast egenskaperna name och value ur en rad andra egenskaper som field har. Mer om destructuring https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment */
-
-  /* Name är det name-attribut som finns på HTML-taggen. title i detta exempel: <input type="text" id="title" name="title" /> 
-  value är innehållet i fältet, dvs. det någon skrivit. */
   const { name, value } = field;
 
-  /* Sätter en variabel som framöver ska hålla ett valideringsmeddelande */
-  let = validationMessage = '';
-  
+  let = validationMessage = "";
   switch (name) {
-    /* Så de olika fallen - case - beror på vilket name-attribut som finns på det elementet som skickades till validateField - alltså vilket fält som någon skrev i eller lämnade. */
-
-    /* Fallet om någon skrev i eller lämnade fältet med name "title" */
-    case 'title': {
-      /* Då görs en enkel validering på om värdet i title-fältet är kortare än 2 */
+    case "title": {
       if (value.length < 2) {
-        /* Om det inte är två tecken långt kommer man in i denna if-sats och titleValid variabeln sätts till false, validationMessage sätts till ett lämpligt meddelande som förklarar vad som är fel.  */
         titleValid = false;
         validationMessage = "Fältet 'Titel' måste innehålla minst 2 tecken.";
       } else if (value.length > 100) {
-        /* Validering görs också för att kontrollera att texten i fältet inte har fler än 100 tecken. */
         titleValid = false;
         validationMessage =
           "Fältet 'Titel' får inte innehålla mer än 100 tecken.";
       } else {
-        /* Om ingen av dessa if-satser körs betyder det att fältet är korrekt ifyllt. */
         titleValid = true;
       }
       break;
     }
-    /* Fallet om någon skrev i eller lämnade fältet med name "description" */
-    case 'description': {
-      /* Liknande enkla validering som hos title */
+    case "description": {
       if (value.length > 500) {
         descriptionValid = false;
         validationMessage =
@@ -56,11 +44,8 @@ function validateField(field) {
       }
       break;
     }
-    /* Fallet om någon skrev i eller lämnade fältet med name "dueDate" */
-    case 'dueDate': {
-      /* Här är valideringen att man kollar om något alls har angetts i fältet. dueDate är obligatoriskt därför måste det vara mer än 0 tecken i fältet */
+    case "dueDate": {
       if (value.length === 0) {
-        /* I videon för lektion 6 är nedanstående rad fel, det står där descriptionValid =  false;, men ska förstås vara dueDateValid = false; */
         dueDateValid = false;
         validationMessage = "Fältet 'Slutförd senast' är obligatorisk.";
       } else {
@@ -69,114 +54,134 @@ function validateField(field) {
       break;
     }
   }
+
   field.previousElementSibling.innerText = validationMessage;
-  
-  field.previousElementSibling.classList.remove('hidden');
+  field.previousElementSibling.classList.remove("hidden");
 }
 
-/* Callbackfunktion som används för eventlyssnare när någon klickar på knappen av typen submit */
 function onSubmit(e) {
   e.preventDefault();
   if (titleValid && descriptionValid && dueDateValid) {
-    console.log('Submit');
+    console.log("Submit");
 
     saveTask();
   }
 }
+
 function saveTask() {
-    const task = {
-      title: todoForm.title.value,
-      description: todoForm.description.value,
-      dueDate: todoForm.dueDate.value,
-      completed: false
-    };
-   
-  
-    api.create(task).then((task) => {
-     
-      if (task) {
-        renderList();
-      }
-    });
-  }
-  
-  function renderList() {
-   
-    console.log('rendering');
-    api.getAll().then((tasks) => {
-      
-      todoListElement.innerHTML = '';
-  
-      
-      if (tasks && tasks.length > 0) {
-        
-        tasks.forEach((task) => {
-          
-  
-          todoListElement.insertAdjacentHTML('beforeend', renderTask(task));
-        });
-      }
-    });
-  }
-  
-  
-  function renderTask({ id, title, description, dueDate }) {
-    
-    let html = `
-      <li class="select-none mt-2 py-2 border-b border-amber-300">
-        <div class="flex items-center">
-          <h3 class="mb-3 flex-1 text-xl font-bold text-pink-800 uppercase">${title}</h3>
-          <div>
-            <span>${dueDate}</span>
-            <button onclick="deleteTask(${id})" class="inline-block bg-amber-500 text-xs text-amber-900 border border-white px-3 py-1 rounded-md ml-2">Ta bort</button>
-          </div>
-        </div>`;
-  
-    description &&
-      
-      (html += `
-        <p class="ml-8 mt-2 text-xs italic">${description}</p>
-    `);
-  
-    html += `
-      </li>`;
-    
-    return html;
-  }
-  
-  function deleteTask(id) {
-    
-  
-    
-    api.remove(id).then((result) => {
-      
-  
+  const task = {
+    title: todoForm.title.value,
+    description: todoForm.description.value,
+    dueDate: todoForm.dueDate.value,
+    completed: false,
+  };
+
+  api.create(task).then((task) => {
+    if (task) {
       renderList();
-      
-    });
+    }
+  });
+}
+
+function renderList() {
+  console.log("rendering");
+
+  api.getAll().then((tasks) => {
+    todoListElement.innerHTML = "";
+
+    if (tasks && tasks.length > 0) {
+      tasks.forEach((task) => {
+        todoListElement.insertAdjacentHTML("beforeend", renderTask(task));
+        var taskItem = document.getElementById(`task-${task.id}`);
+        var checkbox = document.getElementById(`checkbox-${task.id}`);
+        if (task.completed) {
+          taskItem.style.backgroundColor = "Green";
+        } else {
+          taskItem.style.backgroundColor = "Inherit";
+        }
+        checkbox.checked = task.completed;
+      });
+    }
+  });
+}
+
+function renderTask(task) {
+  const JSONData = JSON.stringify(task);
+  let html = `
+    <li id="task-${task.id}" class="select-none mt-2 py-2 border-b border-amber-300">
+      <div class="flex items-center">
+        <h3 class="mb-3 flex-1 text-xl font-bold text-pink-800 uppercase">${task.title}</h3>
+        <div>
+          <span>${task.dueDate}</span>
+          <button onclick="deleteTask(${task.id})" class="inline-block bg-amber-500 text-xs text-amber-900 border border-white px-3 py-1 rounded-md ml-2">Ta bort</button>
+        </div>
+      </div>`;
+
+  /**/
+  description &&
+    (html += `
+      <p class="ml-8 mt-2 text-xs italic ">${task.description}</p>
+      <div class="flex items-center pl-4">
+    <input id="checkbox-${task.id}" onclick='checkboxstate(this,${JSONData})' type="checkbox" value="" name="bordered-checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+    <label for="-checkbox" class="py-4 ml-2 w-full text-1 font-bold text-pink-800 italic ">Utförd</label>
+    </div>
+    
+  `);
+
+  html += `
+    </li>`;
+
+  /***********************Labb 2 ***********************/
+  /* I ovanstående template-sträng skulle det vara lämpligt att sätta en checkbox, eller ett annat element som någon kan klicka på för att markera en uppgift som färdig. 
+  Det elementet bör, likt knappen för delete, också lyssna efter ett event (om du använder en checkbox, kolla på exempelvis w3schools vilket element som triggas hos en checkbox när dess värde förändras.). Skapa en eventlyssnare till det event du finner lämpligt. 
+  Funktionen behöver nog ta emot ett id, så den vet vilken uppgift som ska markeras som färdig. Det skulle kunna vara ett checkbox-element som har attributet on[event]="updateTask(id)". */
+  /***********************Labb 2 ***********************/
+
+  return html;
+}
+
+function checkboxstate(checkbox, task) {
+  var taskItem = document.getElementById(`task-${task.id}`);
+  if (checkbox.checked) {
+    taskItem.style.backgroundColor = "Green";
+  } else {
+    taskItem.style.backgroundColor = "Inherit";
   }
+  task.completed = checkbox.checked;
+  updateTask(task);
+}
+
+function deleteTask(id) {
+  api.remove(id).then((result) => {
+    renderList();
+  });
+}
+
+function updateTask(data) {
+  api.update(data).then((result) => {});
+}
+
+/***********************Labb 2 ***********************/
+/* Här skulle det vara lämpligt att skriva den funktion som angivits som eventlyssnare för när någon markerar en uppgift som färdig. Jag pratar alltså om den eventlyssnare som angavs i templatesträngen i renderTask. Det kan t.ex. heta updateTask. 
   
-  function addTask() {
-    const task = {
-      title: todoForm.title.value,
-      description: todoForm.description.value,
-      dueDate: todoForm.dueDate.value,
-      completed: false
-    };
-  
-    api.create(task).then((task) => {
-      if (task) {
-        renderList();
-      }
-    });
-  }
-  //organizing the tasks when it marked ass done and when they are not done
-  function organizeTodoLists(tasks) {
-    let list = tasks.filter((task) => !task.completed); 
-    let completed = tasks.filter((task) => task.completed); 
-    list.sort(sortByDate); 
-    completed.sort(sortByDate); 
-    list = list.concat(completed); 
-    return list; 
-  }
-  renderList();
+Funktionen bör ta emot ett id som skickas från <li>-elementet.
+*/
+
+/* Inuti funktionen kan ett objekt skickas till api-metoden update. Objektet ska som minst 
+innehålla id på den uppgift som ska förändras, samt egenskapen completed som true eller false, 
+beroende på om uppgiften markerades som färdig eller ofärdig i gränssnittet. 
+Det finns några sätt att utforma det som ska skickas till api.update-metoden. 
+Alternativ 1: objektet består av ett helt task-objekt, som också inkluderar förändringen. 
+Exempel: {id: 1,  title: "x", description: "x", dueDate: "x", completed: true/false}
+Alternativ 2: objektet består bara av förändringarna och id på den uppgift som ska förändras. Exempel: {id: 1, completed: true/false } 
+Om du hittar något annat sätt som funkar för dig, använd för all del det, så länge det uppnår samma sak. :)
+*/
+
+/* Anropet till api.update ska följas av then(). then() behöver, som bör vara bekant vid det här laget, 
+en callbackfunktion som ska hantera det som kommer tillbaka från servern via vår api-klass. 
+Inuti den funktionen bör listan med uppgifter renderas på nytt, så att den nyligen gjorda förändringen syns. */
+
+/***********************Labb 2 ***********************/
+
+/* Slutligen. renderList anropas också direkt, så att listan visas när man först kommer in på webbsidan.  */
+renderList();
